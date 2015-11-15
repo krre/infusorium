1333a27cfc919c84eb56c5f12934f8738855ef1c
Vladimir Zarypov <krre31@gmail.com>
2015-11-15 14:29:17 +0200

    added message functions in dialog.js

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -8,4 +8,18 @@ function selectDirectory(parent) {
     return Utils.createDynamicObject(parent, "qrc:/qml/components/filedialog/FileDialogDirectory.qml")
 }
 
+function infoMessage(message) {
+    return Utils.createDynamicObject(mainRoot, "qrc:/qml/components/messagedialog/MessageDialogInformation.qml", { text: message })
+}
 
+function warningMessage(message) {
+    return Utils.createDynamicObject(mainRoot, "qrc:/qml/components/messagedialog/MessageDialogWarning.qml", { text: message })
+}
+
+function questionMessage(message) {
+    return Utils.createDynamicObject(mainRoot, "qrc:/qml/components/messagedialog/MessageDialogQuestion.qml", { text: message })
+}
+
+function errorMessage(message) {
+    return Utils.createDynamicObject(mainRoot, "qrc:/qml/components/messagedialog/MessageDialogError.qml", { text: message })
+}
