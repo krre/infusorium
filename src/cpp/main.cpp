403db76e64dc87cb0e9774332fbef3171e9cc217
Vladimir Zarypov <krre31@gmail.com>
2024-01-27 11:03:59 +0200

    Use Application class instead QApplication

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -6,27 +6,24 @@
 #include <QtQml>
 
 int main(int argc, char* argv[]) {
-    QApplication appication(argc, argv);
+    Application appication(argc, argv);
     appication.setApplicationName("Infusorium");
-    appication.setApplicationVersion(Application::version());
 
     qmlRegisterType<Process>("Infusorium", 1, 0, "Process");
 
-    Application app;
     Utils utils;
     QString filePath = qApp->applicationDirPath() + "/aquarium.ini";
     Settings settings(filePath);
+
     if (!QFile::exists(filePath)) {
         settings.setValue("Infusoria", "address", "localhost");
         settings.setValue("Infusoria", "port", "51000");
     }
 
     QQmlApplicationEngine engine;
-
-    engine.rootContext()->setContextProperty("APP", &app);
+    engine.rootContext()->setContextProperty("APP", &appication);
     engine.rootContext()->setContextProperty("Settings", &settings);
     engine.rootContext()->setContextProperty("UTILS", &utils);
-
     engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
 
     return appication.exec();
