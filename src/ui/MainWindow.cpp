4d24baf2634df6cc37b6c3bdfcab83ef05909865
Vladimir Zarypov <krre31@gmail.com>
2025-06-21 07:26:21 +0300

    Add `About` dialog

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -1,5 +1,7 @@
 #include "MainWindow.h"
+#include "core/Application.h"
 #include <QMenuBar>
+#include <QMessageBox>
 #include <QCoreApplication>
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
@@ -7,7 +9,22 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
     createActions();
 }
 
+void MainWindow::showAbout() {
+    QMessageBox::about(this, tr("About %1").arg(Application::Name),
+tr(R"(<h3>%1 %2</h3>
+Environment for Infusoria artifical life units<br><br>
+Based on Qt %3<br>
+Build on %4 %5<br><br>
+<a href=%6>%6</a><br><br>
+Copyright © %7, Vladimir Zarypov)")
+        .arg(Application::Name, Application::Version, QT_VERSION_STR,
+        Application::BuildDate, Application::BuildTime, Application::Url, Application::Years));
+}
+
 void MainWindow::createActions() {
     auto fileMenu = menuBar()->addMenu(tr("File"));
     fileMenu->addAction(tr("Exit"), Qt::CTRL | Qt::Key_Q, qApp, &QCoreApplication::quit);
+
+    auto helpMenu = menuBar()->addMenu(tr("Help"));
+    helpMenu->addAction(tr("About %1...").arg(Application::Name), this, &MainWindow::showAbout);
 }
--- src/ui/MainWindow.h
+++ src/ui/MainWindow.h
@@ -6,6 +6,9 @@ class MainWindow : public QMainWindow {
 public:
     MainWindow(QWidget* parent = nullptr);
 
+private slots:
+    void showAbout();
+
 private:
     void createActions();
 };
