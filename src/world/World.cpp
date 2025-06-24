8eaab3150bbd7e5d3e8748e02305089d225ba206
Vladimir Zarypov <krre31@gmail.com>
2025-06-24 05:40:34 +0300

    Add property getters in `World` class

--- src/world/World.cpp
+++ src/world/World.cpp
@@ -7,10 +7,22 @@ World::World(QObject* parent) : QObject(parent) {
 
 void World::create(const QString& name, const QString& directory, quint32 age) {
     qDebug() << "name" << name << "directory" << directory << "age" << age;
+
+    m_name = name;
+    m_dir = name + "/" + directory;
+    m_age = age;
 }
 
 bool World::isRunning() const {
     return m_running;
+}
+
+QString World::name() const {
+    return m_name;
+}
+
+quint32 World::age() const {
+    return m_age;
 }
 
 void World::run() {
--- src/world/World.h
+++ src/world/World.h
@@ -8,10 +8,17 @@ public:
     void create(const QString& name, const QString& directory, quint32 age);
     bool isRunning() const;
 
+    QString name() const;
+    quint32 age() const;
+
 public slots:
     void run();
     void stop();
 
 private:
+    QString m_name;
+    QString m_dir;
+    quint32 m_age = 0;
+
     bool m_running = false;
 };
